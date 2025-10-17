a7d07e5338355dc2294f9ee5a80ca04482f71f91
bryancoloma <brycoloma@gmail.com>
2025-10-17 12:23:58 -0700

    initial commit for pdf_scraper project

---
+++ pdf_scraper.py
@@ -0,0 +1,8 @@
+import requests
+from b4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from pyPDF2 import PdfMerger
+import os
+from pathlib import Path
